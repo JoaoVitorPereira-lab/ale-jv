8a8efe06d68cf1c69deeaa7f1a8ae70fa34a949b
João Vitor <v17112005@gmail.com>
2022-08-17 18:09:36 -0300

    segundo

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import SessionRenderizacao from './pages/session'
+import SessionRenderizacao from './pages/renderizacaoo'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
